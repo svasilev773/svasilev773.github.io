e30905921777418a8cb0939b9d2fa1503add8d45
svasilev773 <115039741+svasilev773@users.noreply.github.com>
2022-10-09 22:36:36 +0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const audio = document.querySelector('#audio');
-const btnka = document.querySelector('#btnka');
-
-btnka.addEventListener('click', () => {
-    audio.paused ? audio.play() : audio.pause();
-});
